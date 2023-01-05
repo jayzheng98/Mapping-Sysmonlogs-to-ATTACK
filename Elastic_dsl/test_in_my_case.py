9569e109796a58dc1b027d0927fee4e6243cd297
Jay Zheng <68698451+jayzheng98@users.noreply.github.com>
2023-01-05 16:42:49 +0800

    Update test_in_my_case.py

--- Elastic_dsl/test_in_my_case.py
+++ Elastic_dsl/test_in_my_case.py
@@ -4,7 +4,7 @@ import json
 import re
 from pandas import DataFrame
 
-
+# The specific index name in ELK. You can use wildcard "*" here as well 
 index1 = 'test-2022.12.15'
 
 tactic_list = ['collection.csv', 'command and control.csv', 'credential access.csv', 'defense evasion.csv',
@@ -15,17 +15,18 @@ def es_search(dsl_term, index_term):
     es = Elasticsearch()
     result = es.search(index=index_term, body=dsl_term, size=300)
     log_list = json.dumps(result["hits"]["hits"], indent=2, ensure_ascii=False)
-    log_list = json.loads(log_list)  # 转换成列表
+    log_list = json.loads(log_list)  # Change to "list" format
     return log_list
 
+# Mapping sysmonlogs to ATT&CK techniques
 def log_labeling():
     output={}
     for file in tactic_list:
-        with open(file, 'r', encoding='utf-8-sig') as dsl_list:  # encoding='utf-8'
+        with open(file, 'r', encoding='utf-8-sig') as dsl_list:
             row = csv.reader(dsl_list)
             for each in row:
-                dsl = eval(each[1])  # 将dsl语句转换为字典
-                logs = es_search(dsl, index1)  # 执行搜索，返回匹配结果（列表里嵌套字典）
+                dsl = eval(each[1])            # Transform the dsl query sentences into "dict" format
+                logs = es_search(dsl, index1)  # Execute queries, the output is a "list" of "dicts"
 
                 for log in logs:
                     try:
@@ -34,36 +35,39 @@ def log_labeling():
                     except:
                         output[log['_id']]=[]
                         output[log['_id']].append(each[0])
-    return output
+    return output                             # {'_id1' : [technique_id1, technique_id3], '_id2' : [technique_id2],...}
 
+# Fetch all logs in a index and export them to a csv file
 def fetch_all(index_term, labelled_logs):
     es = Elasticsearch()
     query = es.search(index=index_term, scroll='5m', size=100)
-    results = query['hits']['hits']  # es查询出的结果第一页
-    total = query['hits']['total']['value']  # es查询出的结果总量
-    scroll_id = query['_scroll_id']  # 游标用于输出es查询出的所有结果
+    results = query['hits']['hits'] 
+    total = query['hits']['total']['value']  
+    scroll_id = query['_scroll_id']
 
     for i in range(0, int(total / 100) + 1):
         query_scroll = es.scroll(scroll_id=scroll_id, scroll='5m')['hits']['hits']
         results += query_scroll
 
     with open('syslog.csv', 'w', newline='', encoding='utf-8') as f:
-        header = ['_id', 'CommandLine', 'EventTime', 'LocalIP', '@timestamp', 'AccountType', 'CallTrace',
+        # Fields of syslog that we want to write into csv file as headers
+        headers = ['_id', 'CommandLine', 'EventTime', 'LocalIP', '@timestamp', 'AccountType', 'CallTrace',
                   'Category', 'Channel', 'CreationUtcTime', 'CurrentDirectory', 'Description', 'EventID',
                   'EventReceivedTime', 'EventType', 'ExecutionProcessID', 'ExecutionThreadID', 'GrantedAccess',
                   'Hashes', 'Hostname', 'Image', 'ImageLoaded', 'IntegrityLevel', 'Keywords', 'LevelValue',
                   'log_from', 'LogonGuid', 'OpcodeValue', 'OriginalFileName', 'ParentCommandLine', 'ParentImage',
-                   'ParentProcessGuid', 'ParentProcessId', 'ParentProcessID',
-                  'ParentUser', 'port', 'ProcessGuid', 'ProcessID', 'ProviderGuid', 'RecordNumber', 'RuleName',
-                  'SchemaVersion', 'Severity', 'SeverityValue', 'Signature', 'SignatureStatus', 'Signed', 'SourceImage',
-                  'SourceModuleName', 'SourceName', 'SourceProcessGUID', 'SourceProcessId', 'SourceThreadId',
-                  'SourceUser', 'SyslogFacility', 'SyslogFacilityValue', 'SyslogSeverity',
-                  'SyslogSeverityValue', 'TargetFilename', 'TargetImage', 'TargetObject', 'TargetProcessGUID',
-                  'TargetProcessId', 'TargetUser', 'Task', 'TaskValue','TerminalSessionId', 'ThreadID', 'User',
-                  'UserID',  'Version', 'RiskLevel']
+                  'ParentProcessGuid', 'ParentProcessId', 'ParentProcessID', 'ParentUser', 'port', 'ProcessGuid', 
+                  'ProcessID', 'ProviderGuid', 'RecordNumber', 'RuleName', 'SchemaVersion', 'Severity', 
+                  'SeverityValue', 'Signature', 'SignatureStatus', 'Signed', 'SourceImage', 'SourceModuleName', 
+                  'SourceName', 'SourceProcessGUID', 'SourceProcessId', 'SourceThreadId', 'SourceUser', 
+                  'SyslogFacility', 'SyslogFacilityValue', 'SyslogSeverity', 'SyslogSeverityValue', 'TargetFilename', 
+                  'TargetImage', 'TargetObject', 'TargetProcessGUID', 'TargetProcessId', 'TargetUser', 'Task', 
+                  'TaskValue','TerminalSessionId', 'ThreadID', 'User', 'UserID',  'Version', 'RiskLevel']
         count = 0
         d = {}
-        for i, name in enumerate(header):
+        
+        # Every value in dict "d" is a "list"
+        for i, name in enumerate(headers):
             d[name] = []
 
         for res in results:
@@ -72,7 +76,7 @@ def fetch_all(index_term, labelled_logs):
                     res['_source']['RiskLevel'] = 1
                     temp = re.findall(r"id=(.+?),", res['_source']['RuleName'], flags=re.IGNORECASE)
                     if temp:
-                        if temp[0] == 'Port Monitors':
+                        if temp[0] == 'Port Monitors':    # To fix a bug of our sysmon's config file
                             temp[0] = 'T1547.010'
                         res['_source']['RuleName'] = temp
                 else:
@@ -81,7 +85,7 @@ def fetch_all(index_term, labelled_logs):
                 res['_source']['RuleName'] = '-'
                 res['_source']['RiskLevel'] = 0
 
-            # Precise matching
+            # Precise matching (Rewrite the "RuleName" and "RiskLevel" of some logs)
             if count < len(labelled_logs):    # If all elements in "labelled_logs" have been matched, skip this process
                 for key, value in labelled_logs.items():
                     if res['_id'] == key:
@@ -89,19 +93,16 @@ def fetch_all(index_term, labelled_logs):
                         res['_source']['RuleName'] = value
                         res['_source']['RiskLevel'] = 2
 
-            # Rewrite a dict for csv
-            d[header[0]].append(res[header[0]])
-            for i in range(1, len(header)):
+            # Construct the dict "d" for the exporting into csv
+            d[headers[0]].append(res[headers[0]])
+            for i in range(1, len(headers)):
                 try:
-                    if(res['_source'][header[i]]!=None):
-                        d[header[i]].append(res['_source'][header[i]])
-                    else:
-                        d[header[i]].append(None)
+                    d[headers[i]].append(res['_source'][headers[i]])
                 except:
-                    d[header[i]].append(None)
+                    d[headers[i]].append(None)
 
         # Output to "syslog.csv"
-        output = DataFrame(d, columns=header)
+        output = DataFrame(d, columns=headers)
         output.to_csv(f, header=True, index=False, encoding='utf-8')
 
 if __name__ == '__main__':
